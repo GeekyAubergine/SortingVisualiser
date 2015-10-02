45e705a3451efe15919da293d52e1b9dfa433f94
GeekyAubergine <c.aubergine@gmail.com>
2015-10-02 18:45:35 +0100

    Started main script, added constants for ID's and Class names

---
+++ sorting-visualiser.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Element ID's
+const GRAPH_ID = 'sorting-visualiser-graph';
+const BUTTONS_CONTAINER_ID = 'sorting-visualiser-buttons-container';
+
+//Bar class names
+const BAR_NORMAL_CLASS = 'bar-normal';
+const BAR_ACTIVE_CLASS = 'bar-active'; //Current bar indexed
+const BAR_COMPARISON_CLASS = 'bar-comparison';
+const BAR_BOUND_CLASS = 'bar-bound';
